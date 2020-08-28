eca4719b416ed84c39dbeb84c18a6304d5dbdfdd
kwilkins <kwilk1984@gmail.com>
2020-08-28 17:14:22 -0500

    Added comments to WeeklyMaintenance and echo statements to output process status.

--- WeeklyMaintenance.ps1
+++ WeeklyMaintenance.ps1
@@ -1,6 +1,20 @@
-﻿set-location 'c:\users'
+﻿#This script will do 
+
+#Clear all cache and temporary files
+echo "Clearing Cache and Temporary Files . . ."
+set-location 'c:\users'
 remove-item '.\*\appdata\local\temp\*' -recurse -force -ErrorAction SilentlyContinue
+
+#Empty the Recycle Bin
+echo "Emptying the Recycle Bin . . ."
 clear-recyclebin -force
+
+#Get and install windows updates
+echo "Updating Windows . . ."
 get-windowsupdate -force
+echo "Installing Windows updates . . ."
 install-windowsupdate -force
+
+#Restart computer to apply updates
+echo "Restarting computer . . ."
 restart-computer -force
