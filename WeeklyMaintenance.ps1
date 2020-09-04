39f1fd459b115fd5d6aeb5249198d9e9beda3ecd
kwilkins <kwilk1984@gmail.com>
2020-09-03 22:24:25 -0500

    Added script to clean up Downloads directory.

--- WeeklyMaintenance.ps1
+++ WeeklyMaintenance.ps1
@@ -1,24 +1,29 @@
 ﻿#This script will do four things:
 ## Clear cache from temporary files
+## Remove files in Downloads directory
 ## Empty the Recycle bin
 ## Get and install Windows updates
 ## Restart the computer to apply updates
 
-#Clear cache and temporary files
+# Clear cache and temporary files
 echo "Clearing Cache and Temporary Files . . ."
 Set-Location 'c:\users'
 Remove-Item '.\*\appdata\local\temp\*' -Recurse -Force -ErrorAction SilentlyContinue
+
+# Remove files in Downloads directory
+Set-Location 'C:\users\kwilk\Downloads'
+Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
 
 #Empty the Recycle Bin
 echo "Emptying the Recycle Bin . . ."
 Clear-RecycleBin -Force
 
-#Get and install windows updates
+# Get and install windows updates
 echo "Updating Windows . . ."
 Get-WindowsUpdate -ForceDownload
 echo "Installing Windows updates . . ."
 Install-WindowsUpdate -ForceInstall
 
-#Restart computer
+# Restart computer
 echo "Restarting computer . . ."
 Restart-Computer -force
