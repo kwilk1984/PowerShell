e332471cf8c34c2596679deabc69583267802b82
kwilkins <kwilk1984@gmail.com>
2021-02-06 11:21:16 -0600

    Adding script to install 'neofetch'

---
+++ NeoFetchInstall.ps1
@@ -0,0 +1,12 @@
+﻿# Neofetch install
+
+# This script will install neofetch to a Windows 10 computer using
+# the script from Scoop
+
+# The user will need to make sure they have the correct
+# Execution-Policy configured in Powershell in order to execute the
+# script.
+
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+scoop install git
+scoop install neofetch
